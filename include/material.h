fc5fde23d959b24ebc88b33c400345854f2a995a
aimtanc <eeyts4@nottingham.ac.uk>
2018-11-26 12:57:52 +0000

    modified

--- include/material.h
+++ include/material.h
@@ -1,29 +1,33 @@
-#include <string>
+//fnc declare//
+#include <iostream>
+#include <string>//string//
 using namespace std;
-
-#ifndef MATERIAL_H
 #define MATERIAL_H
+#ifndef MATERIAL_H //if not define//
 
+//class//
 class Material {
-private:
+private://no change outwhere in the program//
+//member variables//
   double ID;
   double Density;
   string Colour;
   string Name;
 
-public:
-  //construct
-  Material()
-  Material( double aID, double Den, string Col, string aName)
-  //deconstruct
-  ~Material()
-  void setMaterial( double aID, double Den, string Col, string aName)
-
+public://declaration//
+  Material();//nor state//
+  Material( double aID, double Den, string Col, string aName);
+  ~Material();
+  //get=return//
   double getID();
   double getDensity();
   string getColour();
   string getName();
 
-
+  void setID( double aID);
+  void setDensity( double Den);
+  void setColour( string Col);
+  void setName( string aName);
 };
+
 #endif
--- src/material.cpp
+++ src/material.cpp
@@ -1,35 +1,37 @@
+//fnc def//
+
 #include "Material.h"
 
-Material::Material(){
-  this->ID=0.0;
-  this->Density=0.0;
-
+Material::Material(){ //class
+  //set member variables to nor state but the string is already equal nor state
+  this->ID=0.0;//initialize
+  this->Density=0.0;//initialize
 }
+
 Material::~Material(){}
 
-Material::Material( double aID, double Den, string Col, string aName){
-    this->ID = aID;
-    this->Density = Den;
-    this->Colour = Col;
-    this->Name = aName;
-  }
-
-void Material::setMaterial( double aID, double Den, string Col, string aName){
-    this->ID = aID;
-    this->Density = Den;
-    this->Colour = Col;
-    this->Name = aName;
-  }
-
-  double Material::getID(){
+double Material::getID(){
     return this->ID;
   }
-  double Material::getDensity(){
+double Material::getDensity(){
     return this->Density;
   }
-  string Material::getColour(){
+string Material::getColour(){
     return this->Colour;
   }
-  string Material::getName(){
+string Material::getName(){
     return this->Name;
+  }
+
+void Material::setID ( double aID){
+    this->ID = aID;
+  }
+void Material::setDensity ( double Den){
+    this->Density = Den;
+  }
+void Material::setColour ( double Col){
+    this->Colour = Col;
+  }
+void Material::setName ( double aName){
+    this->Name = aName;
   }
