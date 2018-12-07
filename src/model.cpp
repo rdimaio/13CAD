1a369b132150c8a0367eae4e71738dc3553b1b52
dmsimuko <dmsimuko@gmail.com>
2018-12-07 16:35:48 +0000

    model v1

---
+++ src/model.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <Model.h>
+#include <iostream>
+
+//Code to read file line by line 
+//Read first letter and sort into Cell, Material, Vector 
+Class:Model
+
+
+//Cell code
+Setcell{};
+GetCell{};
+
+
+//Materia Code
+SetMaterial{};
+GetMaterial{};
+
+
+//Vector Code 
+SetVector{};
+GetCell{};
