194998fe8a8f2a51648464eee80f364e687792dc
post-rick <riccardodimaio11@gmail.com>
2018-12-14 16:26:21 +0100

    Update test

--- tests/test_model.cpp
+++ tests/test_model.cpp
@@ -6,11 +6,12 @@
 
 TEST(modelTest, modelBase) {
 
-	Model m("ExampleModel.mod");
+	Model m("tests/ExampleModel.mod");
 
     std::vector<Material> materials = m.getMaterials();
 
     std::string name1 = materials[0].getName();
+    std::string name2 = materials[1].getName();
 
 	ASSERT_EQ(name1, "test");
-}
+    ASSERT_EQ(name2, "pkg_plastic");
