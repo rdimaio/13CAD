500760ed2663fbf35abe825cbf2b58674b5db303
post-rick <riccardodimaio11@gmail.com>
2019-03-04 14:17:44 +0000

    Add basis for STL/MOD file opening

--- src/gui/mainwindow.cpp
+++ src/gui/mainwindow.cpp
@@ -1,3 +1,4 @@
+// VTK libraries
 #include <vtkSmartPointer.h>
 #include <vtkCubeSource.h>
 #include <vtkActor.h>
@@ -12,31 +13,64 @@
 #include <vtkNew.h>
 #include <vtkGenericOpenGLRenderWindow.h>
 
+// STL reading libraries
+#include <vtkSTLReader.h>
+#include <vtkPolyDataMapper.h>
+
+// Qt headers
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+
+// Local headers
+#include "model.h"
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     // standard call to setup Qt UI (same as previously)
     ui->setupUi( this );
 	
+	//std::string inputFilename = "src/gui/test.stl";
+	std::string inputFilename = "tests/ExampleModel.mod";
+	// Load model
+  	Model model(inputFilename);
+
 	// Now need to create a VTK render window and link it to the QtVTK widget
 	vtkNew<vtkGenericOpenGLRenderWindow> renderWindow;
 	ui->qvtkWidget->SetRenderWindow( renderWindow );			// note that vtkWidget is the name I gave to my QtVTKOpenGLWidget in Qt creator
-	
-	// Now use the VTK libraries to render something. To start with you can copy-paste the cube example code, there are comments to show where the code must be modified.
-	//--------------------------------------------- Code From Example 1 --------------------------------------------------------------------------
-	// Create a cube using a vtkCubeSource
-	vtkSmartPointer<vtkCubeSource> cubeSource = vtkSmartPointer<vtkCubeSource>::New();
 
-	// Create a mapper that will hold the cube's geometry in a format suitable for rendering
-	vtkSmartPointer<vtkDataSetMapper> mapper = vtkSmartPointer<vtkDataSetMapper>::New();
-	mapper->SetInputConnection( cubeSource->GetOutputPort() );
+	vtkSmartPointer<vtkActor> actor;
+
+	if (model.getIsSTL()) {
+  	// Visualize
+	vtkSmartPointer<vtkSTLReader> reader =
+	vtkSmartPointer<vtkSTLReader>::New();
+	reader->SetFileName(inputFilename.c_str());
+	reader->Update();
+
+  	vtkSmartPointer<vtkPolyDataMapper> mapper =
+  	  vtkSmartPointer<vtkPolyDataMapper>::New();
+  	mapper->SetInputConnection(reader->GetOutputPort());
 
 	// Create an actor that is used to set the cube's properties for rendering and place it in the window
-	vtkSmartPointer<vtkActor> actor = vtkSmartPointer<vtkActor>::New();
+	actor = vtkSmartPointer<vtkActor>::New();
 	actor->SetMapper(mapper);
-	actor->GetProperty()->EdgeVisibilityOn();
+
+	} else {
+		// Now use the VTK libraries to render something. To start with you can copy-paste the cube example code, there are comments to show where the code must be modified.
+		//--------------------------------------------- Code From Example 1 --------------------------------------------------------------------------
+		// Create a cube using a vtkCubeSource
+		vtkSmartPointer<vtkCubeSource> cubeSource = vtkSmartPointer<vtkCubeSource>::New();
+
+		// Create a mapper that will hold the cube's geometry in a format suitable for rendering
+		vtkSmartPointer<vtkDataSetMapper> mapper = vtkSmartPointer<vtkDataSetMapper>::New();
+		mapper->SetInputConnection( cubeSource->GetOutputPort() );
+
+		// Create an actor that is used to set the cube's properties for rendering and place it in the window
+		actor = vtkSmartPointer<vtkActor>::New();
+		actor->SetMapper(mapper);
+	}
+
+	//actor->GetProperty()->EdgeVisibilityOn();
 
 	vtkSmartPointer<vtkNamedColors> colors = vtkSmartPointer<vtkNamedColors>::New();
 	actor->GetProperty()->SetColor( colors->GetColor3d("Red").GetData() );
@@ -63,7 +97,7 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
 	// Render and interact
 	//renderWindow->Render();					// ###### Not needed with Qt ######
 	//renderWindowInteractor->Start();			// ###### Not needed with Qt ######
-	//--------------------------------------------- /Code From Example 1 -------------------------------------------------------------------------*/
+
 }
 
 MainWindow::~MainWindow()
