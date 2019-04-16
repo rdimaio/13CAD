c36708e865427c3d58f1a7e8853ffaf95d9ff519
aimtanc <eeyts4@nottingham.ac.uk>
2019-04-16 15:06:25 +0100

    light and camera

--- src/gui/mainwindow.cpp
+++ src/gui/mainwindow.cpp
@@ -226,6 +226,11 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
 	//renderWindowInteractor->SetRenderWindow( ui->vtkWidget->GetRenderWindow() );				
 	renderer->SetBackground( colors->GetColor3d("Grey").GetData() );
 
+        // Setup the light
+        light = vtkSmartPointer<vtkLight>::New();
+        light->SetLightTypeToHeadlight();
+        light->SetIntensity( 1 );
+
 	// Setup the renderers's camera
 	renderer->ResetCamera();
 	renderer->GetActiveCamera()->Azimuth(30);
@@ -241,4 +246,34 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+void MainWindow::on_horizontalSlider_sliderMoved(int position) //light
+{
+    light->SetIntensity((float) (100-position)/100);
+    ui->qvtkWidget->GetRenderWindow()->Render();
+}
+
+void MainWindow::on_comboBox_activated(const QString &arg1) //camera
+{
+    if (arg1 == "X-Axis"){
+        renderer->GetActiveCamera()->SetPosition(1.0,0.0,0.0);
+    }
+    else if (arg1 == "-X-Axis"){
+         renderer->GetActiveCamera()->SetPosition(-1.0,0.0,0.0);
+    }
+    else if (arg1 == "Y-Axis"){
+         renderer->GetActiveCamera()->SetPosition(0.0,1.0,0.0);
+    }
+    else if (arg1 == "-Y-Axis"){
+         renderer->GetActiveCamera()->SetPosition(0.0,-1.0,0.0);
+    }
+    else if (arg1 == "Z-Axis"){
+         renderer->GetActiveCamera()->SetPosition(0.0,0.0,1.0);
+    }
+    else if (arg1 == "-Z-Axis"){
+         renderer->GetActiveCamera()->SetPosition(0.0,0.0,-1.0);
+    }
+    renderer->ResetCamera();
+    ui->qvtkWidget->GetRenderWindow()->Render();
+
 }
